8f0c1ce52ac6c7e6bbcf72c59961cdfb1ea231a6
Yifan Wang <icyazufan@gmail.com>
2018-08-04 20:22:28 +0000

    add DATABASEURL

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ var express = require("express"),
     var commentRoutes = require("./routes/comments"),
         campgroundRoutes = require("./routes/campgrounds"),
         indexRoutes = require("./routes/index")
-    
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
-mongoose.connect("mongodb://yifan:wyff11@ds113522.mlab.com:13522/yelpcamp", { useNewUrlParser: true });
+
+console.log(process.env.DATABASEURL);
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+//mongoose.connect("mongodb://yifan:wyff11@ds113522.mlab.com:13522/yelpcamp", { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
